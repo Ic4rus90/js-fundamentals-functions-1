5a4f2f1c5afbc8b76e60ba9892526b02f79a410a
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-23 11:17:08 +0200

    Finished exercises

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function lowerAndUpper(lower, upper) {
+  const array = []
+  for (let i = lower; i <= upper; i++) {
+    array.push(i)
+  }
+  return array
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -22,6 +29,16 @@
 //
 // TODO: write code below
 
+function rewriteString(string, number) {
+  let exclamationMarks = ''
+  const capitalizedString = string.toUpperCase()
+
+  for (let i = 0; i < number; i++) {
+    exclamationMarks = exclamationMarks + '!'
+  }
+  return capitalizedString.concat(exclamationMarks)
+}
+
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
 //
@@ -34,9 +51,22 @@
 // '23:50', 30  | '00:20'
 // TODO: write code below
 
+function showNewTime(string, minutes) {
+  const timeArray = string.split(':')
+  let existingHours = parseInt(timeArray[0])
+  let existingMinutes = parseInt(timeArray[1])
+
+  if (existingMinutes + minutes >= 60) {
+    existingMinutes -= 60
+    existingHours++
+  }
+
+  return '' + existingHours + ':' + existingMinutes
+}
+
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: lowerAndUpper, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: rewriteString, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: showNewTime // etc
 }
